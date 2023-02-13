c90d88845bde4f3da0756a4e1b7b04404b6f164f
Hori <horacio.maldonado2@gmail.com>
2023-02-13 20:21:33 -0300

    primer avance

---
+++ index.js
@@ -0,0 +1,79 @@
+//1)Utilizando if y else, crear 5 variables de tipo number y evaluar si cada una de esas variables 
+//es par o impar, en caso de ser par, imprimir “la variable es par”, en caso de ser impar el 
+//mensaje deberá decir, “la variable es impar”. 
+
+console.log("Ejercicio 1)")
+//ejemplo1
+const numero_1=5561;
+//ejemplo2
+const numero_2=1233;
+//ejemplo3
+const numero_3=9572;
+//ejemplo4
+const numero_4=2408;
+//ejemplo5
+const numero_5=3913;
+
+if(numero_1%2==0)
+{
+    console.log("el número "+numero_1+" es par");
+} 
+else
+{
+    console.log("el número "+numero_1+" es impar");
+}
+if(numero_2%2==0)
+{
+    console.log("el número "+numero_2+" es par");
+} 
+else
+{
+    console.log("el número "+numero_2+" es impar");
+}
+if(numero_3%2==0)
+{
+    console.log("el número "+numero_3+" es par");
+} 
+else
+{
+    console.log("el número "+numero_3+" es impar");
+}
+if(numero_4%2==0)
+{
+    console.log("el número "+numero_4+" es par");
+} 
+else
+{
+    console.log("el número "+numero_4+" es impar");
+}
+if(numero_5%2==0)
+{
+    console.log("el número "+numero_5+" es par");
+} 
+else
+{
+    console.log("el número "+numero_5+" es impar");
+}
+
+console.log("Ejercicio 2)");
+//Un alumno de programación está creando su primera aplicación web. Crear una variable cuyo valor 
+//sea la etiqueta h1 de html con el mensaje “Hola Mundo” en el medio e imprimirla en la consola.
+
+console.log("Ejercicio 4");
+//Dibujar en la consola una pirámide. 
+
+let p1=".............";
+let p2=" ...........";
+let p3="  .........";
+let p4="   .......";
+let p5="    .....";
+let p6="     ...";
+let p7="      .";
+
+console.log(p7);
+console.log(p6);
+console.log(p5);
+console.log(p4);
+console.log(p3);
+console.log(p2);
+console.log(p1);
